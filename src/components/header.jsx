448f2866c693c145bb25a819292576a6728acea8
aryasoni1 <46000986+aryasoni1@users.noreply.github.com>
2023-08-30 08:20:00 +0530

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+function Bar() {
+  return (
+    <header>
+      <div className="dropdown nav">
+        <a href>HOME</a>
+        <a href>ABOUT</a>
+        <a href>PROJECTS</a>
+        <a href>CONTACTS</a>
+      </div>
+    </header>
+  );
+}
+export default Bar;
